fe631dbb4c2ab63920d72b8b672b70243afce237
Dobromir Ivanov <dobromirivanov1@gmail.com>
2015-09-26 02:16:16 +0300

    Utilities project is renamed

--- App/App.Web/Global.asax.cs
+++ App/App.Web/Global.asax.cs
@@ -1,23 +1,20 @@
 ﻿using App.Utilities;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace App.Web
 {
-    public class MvcApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
-            AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-            ModelBinders.Binders.Add(typeof(DateTime), new BulgarianTimeModelBinder());
-        }
-    }
-} 
+	public class MvcApplication : System.Web.HttpApplication
+	{
+		protected void Application_Start()
+		{
+			AreaRegistration.RegisterAllAreas();
+			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
+			ModelBinders.Binders.Add(typeof(DateTime), new BulgarianTimeModelBinder());
+		}
+	}
+}
